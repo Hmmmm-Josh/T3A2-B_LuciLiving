eec74fdcec9971646f024c657443f6b2b6138700
Hmmmm-Josh <j.nahlous22@gmail.com>
2024-08-13 21:32:10 +1000

    Feat-Header.Added

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -6,17 +6,21 @@ import About from './pages/about'
 import Dasboard from './pages/Dasboard'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Projects from './pages/Projects.Jsx'
+import Header from './compoents/Header'
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/dashboard' element={<Dasboard />} />
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/projects' element={<Projects />} />
         </Routes>
       </BrowserRouter>
     </div>
