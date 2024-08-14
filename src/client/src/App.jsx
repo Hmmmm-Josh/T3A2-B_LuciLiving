e01770b4d478bb5008e0636717944727c6551b1f
Hmmmm-Josh <j.nahlous22@gmail.com>
2024-08-14 22:08:05 +1000

    footer

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home'
-import About from './pages/about'
-import Dasboard from './pages/Dasboard'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Projects from './pages/Projects.Jsx'
-import Header from './compoents/Header'
+import Home from './pages/Home';
+import About from './pages/about';
+import Dasboard from './pages/Dasboard';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Projects from './pages/Projects';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
           <Route path='/signup' element={<SignUp />} />
           <Route path='/projects' element={<Projects />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
