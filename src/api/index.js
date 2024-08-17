c73496dc193b6b873cd6c459ea9882325aaa7af2
Hmmmm-Josh <j.nahlous22@gmail.com>
2024-08-17 16:40:51 +1000

    Feat-create posts

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from './routes/post.route.js';
+import cookieParser from 'cookie-parser';
+
 
 dotenv.config();
 
@@ -13,20 +16,22 @@ mongoose
   }) 
   .catch((err) => {
     console.log(err);
-  });
+});
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(8000, () => {
-  console.log('Server is running on port 8000');
- });
+  console.log('Server is running on port 8000! :3');
+});
 
- app.use("/api/user", userRoutes);
- app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
- app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
@@ -34,4 +39,4 @@ app.listen(8000, () => {
     statusCode,
     message,
   });
- });
+});
