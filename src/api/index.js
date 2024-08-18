7980929dc446933acacb4caaa3b750996a62e765
Hmmmm-Josh <j.nahlous22@gmail.com>
2024-08-18 19:46:03 +1000

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -41,7 +41,7 @@ app.use(epxress.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
